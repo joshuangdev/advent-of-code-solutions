da60e4df18594cd2b3fcf1af39f98c7820c8e598
Joshua Ng <ng_joshua@hotmail.com>
2025-12-08 18:48:32 +0000

    Update solution.py

--- day-5/part2/solution.py
+++ day-5/part2/solution.py
@@ -1,6 +1,6 @@
 from data import x
 
-testing=False
+testing = False
 
 data = (
     x
@@ -19,15 +19,51 @@ data = (
 )
 
 split = data.split("\n\n")
-ranges = split[0].split()
-#entries = split[1].split()
-#fresh = 0
-#print(ranges,entries)
+ranges = list(filter(None, split[0].split()))
 
-for _ in ranges:
-    startRange = int(_.split("-")[0])
-    endRange = int(_.split("-")[1])
-    if startRange <= igr <= endRange:
-        fresh +=1
+def overlapped(r1, r2, tf, list_=None):
+    if tf:
+        return max(r1[0], r2[0]) <= min(r1[1], r2[1])
+    list_.append(f"{min(r1[0], r2[0])}-{max(r1[1], r2[1])}")
 
-print(fresh)
+def parseInput(r1, r2=None):
+    return (
+        (int(r1.split("-")[0]), int(r1.split("-")[1])),
+        (int(r2.split("-")[0]), int(r2.split("-")[1])) if r2 else "",
+    )
+
+tmp = []
+
+p = 0
+
+def checkAll():
+    global ranges
+    merged = []
+    used = set()
+    for i in range(len(ranges)):
+        a,b = map(int, ranges[i].split("-"))
+        for j in range(i+1, len(ranges)):
+            r1,r2 = parseInput(ranges[i], ranges[j])
+            if overlapped(r1,r2,True):
+                overlapped(r1,r2,False, merged)
+                used.add(i)
+                used.add(j)
+                break
+        if i not in used:
+            merged.append(ranges[i])
+    ranges = merged
+    return ranges
+
+
+prev_len = -1
+while prev_len != len(ranges):
+    prev_len = len(ranges)
+    ranges = checkAll()
+
+p = 0
+for r in ranges:
+    start, end = map(int, r.split("-"))
+    p += end - start + 1
+
+
+print(p)
