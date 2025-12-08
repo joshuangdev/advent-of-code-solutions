99a4dedcfe7462efb9c3753a59d3b3729a04153a
Joshua Ng <ng_joshua@hotmail.com>
2025-12-08 07:58:12 +0000

    grinding 08.12.2025 (last night upload)

---
+++ day-5/part1/solution.py
@@ -0,0 +1,35 @@
+from data import x
+
+testing=False
+
+data = (
+    x
+    if not testing
+    else """3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32"""
+)
+
+split = data.split("\n\n")
+ranges = split[0].split()
+entries = split[1].split()
+fresh = 0
+#print(ranges,entries)
+
+for ingredient in entries:
+    igr = int(ingredient)
+    for _ in ranges:
+        startRange = int(_.split("-")[0])
+        endRange = int(_.split("-")[1])
+        if startRange <= igr <= endRange:
+            fresh +=1
+            break
+print(fresh)
